0d9cdca39ba4e7e7f4305d1b8a22d908878cc2a2
Rahul Yadav <ryrahul_yadav@outlook.com>
2023-08-19 00:44:45 +0530

    End of section 3

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,35 @@
+using Polly;
+using Polly.Extensions.Http;
+using SearchService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (System.Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+  => HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
